17fe900de42f5cbfe64a950e734a2b8af07a16fa
Alex Collin <alex@collin.su>
2016-12-31 02:56:50 +0300

    Start dispatcher

--- services/app.py
+++ services/app.py
@@ -1,9 +1,14 @@
 import sys
 import traceback
+import time
+import socket
+import os
+import json
 
 from fixtures.fixtures import Fixtures
-from services.worker import Worker
+from models.worker import Worker
 from providers.providers import Providers
+from services.dispatcher import Dispatcher
 
 
 class App(object):
@@ -12,23 +17,44 @@ class App(object):
     def __init__(self):
         try:
             Fixtures.execute_fixtures()
-            self.worker = Worker.launch()
+            self.launch()
+            dispatcher = Dispatcher(self.worker.id)
+            dispatcher.start_tracking()
         finally:
-            self.exit_handler()
+            ex_type, ex, tb = sys.exc_info()
+            tb_list = traceback.extract_tb(tb)
+            code = "Normal"
+            if ex_type:
+                code = str(ex_type.__name__)
+            description = ""
+            if ex:
+                description = str(ex)
+            self.terminate(code, tb_list, description)
+            if not Providers.config().debug:
+                sys.exit(0)
 
-    def exit_handler(self):
-        ex_type, ex, tb = sys.exc_info()
-        tb_list = traceback.extract_tb(tb)
-        code = "Normal"
-        if ex_type:
-            code = str(ex_type.__name__)
-        description = ""
-        if ex:
-            description = str(ex)
-        Worker.terminate(self.worker, code, tb_list, description)
-        if not Providers.config().debug:
-            sys.exit(0)
+    def launch(self):
+        self.worker = Worker()
+        self.worker.host_name = socket.gethostname()
+        self.worker.pid = os.getpid()
+        self.worker.launched_at = time.time()
+        return self.worker.save()
+
+    def terminate(self, code, traceback, description):
+        self.worker.terminated_code = code
+        traceback_list = []
+        for item in traceback:
+            traceback_list.append({
+                "filename": item.filename,
+                "line_number": item.lineno,
+                "method_name": item.name,
+                "string": item.line
+            })
+        self.worker.terminated_traceback = json.dumps(traceback_list)
+        self.worker.terminated_description = description
+        self.worker.update_on_terminate()
 
     @staticmethod
     def start():
         App()
+
--- services/dispatcher.py
+++ services/dispatcher.py
@@ -1,8 +1,40 @@
+import threading
+import sys
+import traceback
+
+
 class Dispatcher(object):
+    threads_count = 0
     worker_id = None
 
     def __init__(self, worker_id):
         self.worker_id = worker_id
 
     def start_tracking(self):
+        while True:
+            pass
+
+    def start_service(self, service_name, params=None):
+        self.threads_count += 1
+        try:
+            thread = None
+            if service_name == "analyzer":
+                thread = threading.Thread(target=self.test_thread, name=self.threads_count)
+
+            thread.daemon(True)
+            thread.run()
+
+        except BaseException:
+            ex_type, ex, tb = sys.exc_info()
+            tb_list = traceback.extract_tb(tb)
+            code = "Normal"
+            if ex_type:
+                code = str(ex_type.__name__)
+            description = ""
+            if ex:
+                description = str(ex)
+            print(self.threads_count, code, tb_list, description)
+
+    def test_thread(self, params):
+        d
         pass
