8581a179ef6e58887d6c1ff93ecc2887837e0ec9
Alex Collin <alex@collin.su>
2017-01-17 05:09:08 +0300

    Bug fix

--- services/signaler.py
+++ services/signaler.py
@@ -6,17 +6,17 @@ class Signaler:
             all_amounts = pattern.calls_count + pattern.puts_count
             all_condition = all_amounts / 100
             if pattern.calls_count < pattern.puts_count:
-                if pattern.last_call <= -task.settings.signaler_min_repeats:
+                if pattern.last_call <= -task.setting.signaler_min_repeats:
                     if pattern.puts_count / all_condition > task.settings.signaler_min_chance:
                         result = 'put'
             else:
                 if pattern.calls_count > pattern.puts_count:
-                    if pattern.last_call >= task.settings.trader_min_repeats:
-                        if pattern.calls_count / all_condition > task.settings.signaler_min_chance:
+                    if pattern.last_call >= task.setting.trader_min_repeats:
+                        if pattern.calls_count / all_condition > task.setting.signaler_min_chance:
                             result = 'call'
 
-            max_change = task.settings.signaler_max_change_cost
-            min_change = task.settings.signaler_min_change_cost
+            max_change = task.setting.signaler_max_change_cost
+            min_change = task.setting.signaler_min_change_cost
             if pattern.max_change > max_change or pattern.min_change < min_change:
                 result = None
 
