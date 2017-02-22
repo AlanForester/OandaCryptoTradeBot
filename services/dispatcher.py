9b2e96bab3e36607a7ea188265538638f0ce8c0a
Alex Collin <alex@collin.su>
2017-02-22 17:30:19 +0300

    Process

--- services/dispatcher.py
+++ services/dispatcher.py
@@ -5,6 +5,7 @@ from models.task import Task
 from services.analyzer import Analyzer
 from services.history.collector import Collector
 from helpers.exthread import ExThread
+from multiprocessing import Process
 
 
 class Dispatcher(object):
@@ -19,7 +20,6 @@ class Dispatcher(object):
             pending_tasks = Task.get_pending(self.worker_id)
             for task in pending_tasks:
                 thread = ExThread(target=self.start_service, args=(task,))
-                thread.setDaemon(True)
                 thread.start()
             time.sleep(1)
 
@@ -29,17 +29,16 @@ class Dispatcher(object):
         thread = None
         try:
             if task.service_name == "analyzer":
-                thread = ExThread(target=Analyzer.run, args=(task, ), name=len(self.threads))
+                thread = Process(target=Analyzer.run, args=(task, ), name=len(self.threads))
             if task.service_name == "collector":
-                thread = ExThread(target=Collector, args=(task,), name=len(self.threads))
+                thread = Process(target=Collector, args=(task,), name=len(self.threads))
             if task.service_name == "checker":
-                thread = ExThread(target=Checker, args=(task,), name=len(self.threads))
+                thread = Process(target=Checker, args=(task,), name=len(self.threads))
             if task.service_name == "collector_and_checker":
-                thread = ExThread(target=Dispatcher.collector_and_checker, args=(task,), name=len(self.threads))
+                thread = Process(target=Dispatcher.collector_and_checker, args=(task,), name=len(self.threads))
 
             if thread:
                 self.launch_task(task, len(self.threads))
-                thread.task = task
                 thread.run()
         finally:
             self.terminate_task(task)
